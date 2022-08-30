8f0387cea2ca3b879bd08e70ee510fffecd64919
Boof <97455552+hexahigh@users.noreply.github.com>
2022-08-30 12:34:22 +0200

    Fixed setting your iq to 200 would break it

--- iq.lua
+++ iq.lua
@@ -2,9 +2,9 @@ print ("Please input your iq")
 iq=io.read("*n")
 loop = 0
 
-if (iq) >=100 and (iq) < 200 then print ("smart") elseif (iq) < 100 then print ("idiot") elseif (iq) > 200 then 
+if (iq) >=100 and (iq) < 200 then print ("smart") elseif (iq) < 100 then print ("idiot") elseif (iq) >= 200 then 
 repeat
     print("super smart")
     loop = loop + 1
-until( loop >1000 )
+until( loop >900 )
 end
