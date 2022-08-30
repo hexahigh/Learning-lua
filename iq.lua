951557dda87c72c5b65764dc0ccbab14fa2dacbd
Boof <97455552+hexahigh@users.noreply.github.com>
2022-08-30 12:27:22 +0200

    Update iq.lua

--- iq.lua
+++ iq.lua
@@ -1,5 +1,5 @@
 print ("Please input your iq")
 iq=io.read("*n")
 
-if (iq) > 100 then print ("smart") elseif (iq) < 100 then print ("idiot")
+if (iq) >=100 and (iq) < 200 then print ("smart") elseif (iq) < 100 then print ("idiot") elseif (iq) > 200 then print("super smart")
 end
