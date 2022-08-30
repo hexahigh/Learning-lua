aec0b973d339e926abd3c9c25e72a6fe6c105bf1
Boof <97455552+hexahigh@users.noreply.github.com>
2022-08-30 14:08:32 +0200

    Update temp-file.lua

--- temp-file.lua
+++ temp-file.lua
@@ -3,13 +3,17 @@ var2=0
 var3=0
 var4=0
 var5=0
-kill=false
+kill=true
+choice=0
 
 repeat
-print("Choose variable to edit")
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
+    print("Choose variable to edit")
+    print("1")
+    print("2")
+    print("3")
+    print("4")
+    print("5")
+    choice=io.read("*n")
+    if (choice) == 1 then print("This is the current value") print(var1)
 until( kill=true )
+end
