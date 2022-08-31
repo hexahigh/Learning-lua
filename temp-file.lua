8ee5552e7ad2b2c42d8ad4eef337d0740027a821
Boof <97455552+hexahigh@users.noreply.github.com>
2022-08-31 10:14:22 +0200

    Update temp-file.lua

--- temp-file.lua
+++ temp-file.lua
@@ -3,10 +3,11 @@ var2=0
 var3=0
 var4=0
 var5=0
-kill=true
+lla=0
 choice=0
 
 repeat
+    lla=lla + 1
     print("Choose variable to edit")
     print("1")
     print("2")
@@ -14,6 +15,6 @@ repeat
     print("4")
     print("5")
     choice=io.read("*n")
-    if (choice) == 1 then print("This is the current value") print(var1)
-until( kill=true )
+    if (choice) == 1 then print("This is the current value, would you like to change it? Y and N") print(var1) cvar1=io.read("*all") if  cvar1==true then print("please input new value/data") var1=io.read("*all")
+until( lla==99999 )
 end
